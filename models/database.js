8b102433fbb10a5def57487a7dade9b0372f4378
Mohamad <mo-hasan@hotmail.com>
2021-04-04 22:34:19 +0300

    Updates

--- models/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.mongodbURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-module.exports = mongoose;
