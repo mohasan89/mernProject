acd8ef3a94b7793b7ad96c7777de51c91a579746
Mohamad <mo-hasan@hotmail.com>
2021-04-04 22:43:57 +0300

    Updates

--- frontend/src/store/constants/cartConstants.js
+++
@@ -1,7 +0,0 @@
-export const addItemToCart = "ADD_ITEM_TO_CART";
-export const deleteItemFromCart = "DELETE_ITEM_FROM_CART";
-export const editItemFromCart = "EDIT_ITEM_FROM_CART";
-
-export const SAVE_SHIPPING_ADDRESS = "SAVE_SHIPPING_ADDRESS";
-
-export const SAVE_PAYMENT_METHOD = "SAVE_PAYMENT_METHOD";
